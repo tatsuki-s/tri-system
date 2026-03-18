a50c47ff0bec5cc0b268b606e268ee0d1df164d8
tatsuki-s <zekoushn@gmail.com>
2026-03-18 18:23:06 +0900

    鯖側がただの繰り返しをやめるようにｓｈちあ

--- server/server.mjs
+++ server/server.mjs
@@ -1,6 +1,12 @@
 import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8000});
+let jsonData
+
+let activeData = {
+  trains: [],
+  points: []
+}
 
 wss.on('connection', (ws) => {
   ws.on('error', console.error);
@@ -9,6 +15,13 @@ wss.on('connection', (ws) => {
     console.log('received: %s', data);
     wss.clients.forEach((client) => {
       if(client.readyState === 1){
+        jsonData = JSON.parse(data)
+        console.log("json:",jsonData)
+        if (jsonData.type === "camera"){
+          activeData.trains.push(jsonData) 
+          console.log(activeData)
+        }
+
         client.send(data.toString());
       }
     });
