ae4176906d588fb7352e2a60a5c9fbf19c27ecfe
tatsuki-s <zekoushn@gmail.com>
2026-04-13 18:03:31 +0900

    運転台モニターで必要情報のみ表示するようにした

--- frontend/src/views/Train.vue
+++ frontend/src/views/Train.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
-//import { defineProps } from 'vue'
-const props = defineProps({
-  trainData: String
-})
+import { inject } from "vue"
+const data = inject<any>("wsData")
 </script>
 <template>
   <p>ここに運転台モニター</p>
-  <p>{{props.trainData}}</p>
+  <!-- <p>{{data}}</p> -->
+  <div>
+    <div v-for="train in data.trains">
+      <div v-if="train.id.toString() === $route.params.id">
+        <p>{{train}}</p>
+      </div>
+    </div>
+  </div>
 </template>
--- frontend/src/views/TrainList.vue
+++ frontend/src/views/TrainList.vue
@@ -12,8 +12,8 @@ console.log(data)
   <h1>車両一覧</h1>
   <ul v-if="data">
     <li v-for="train in data.trains"> 
-      <RouterLink :to="`train-list/${train.id}`" :trainData="train">
-      id: {{train.id}}, 現在位置：{{train.read_id}}
+      <RouterLink :to="`train-list/${train.id}`">
+        id: {{train.id}}, 現在位置：{{train.read_id}}
       </RouterLink>
     </li>
   </ul>
