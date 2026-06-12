30d1e99872bd081caaf63f25ba0efdda0da09a01
tatsuki-s <zekoushn@gmail.com>
2026-06-12 19:58:07 +0900

    mqttのトピックを読めるようにした

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,43 +3,46 @@ import { onMounted, onUnmounted, ref, provide } from 'vue'
 import { RouterView } from 'vue-router'
 import  mqtt from 'mqtt'
 
-const mqttData = ref(null)
+const mqttData = ref<any>(null)
 const mqttUrl = import.meta.env.MQTT_HOST || "localhost"
 const mqttPort = 9001
 
 let client: mqtt.MqttClient | null = null
 
+provide('mqttData', mqttData)
+
 onMounted(() =>{
-  const url = `mqtt://${mqttUrl}:${mqttPort}`
+  const url = `ws://${mqttUrl}:${mqttPort}`
   client = mqtt.connect(url, {
-    keepalive: 60,
+    keepalive: 5, //5sec
     clientId: 'vue-' + Math.random().toString(16).substring(2, 8),
   })
 
   client.on('connect', () => {
     console.log("Connected")
-    client?.subscribe('test/pico', (err: any) => {
-      if(!err){
-        console.log('subscribed')
-      }
-    })
+    mqttData.value = client
+
+    //client?.subscribe('test/pico', (err: any) => {
+    //  if(!err){
+    //    console.log('subscribed')
+    //  }
+    //})
   })
 
-  client.on('message', (topic: any, message: any) => {
+  //client.on('message', (topic: any, message: any) => {
     // messageはBufferなので文字列に変換してパース
-    console.log(`Topic: ${topic}, Message: ${message.toString()}`)
-    try {
-      mqttData.value = JSON.parse(message.toString())
-    } catch (e) {
-      mqttData.value = message.toString()
-    }
-  })
+  //  console.log(`Topic: ${topic}, Message: ${message.toString()}`)
+  //  try {
+  //    mqttData.value = JSON.parse(message.toString())
+  //  } catch (e) {
+  //    mqttData.value = message.toString()
+  //  }
+  //})
 
   client.on('error', (err: any) => {
     console.error('MQTT Connection Error:', err)
   })
 
-  provide('mqttData', mqttData)
 
 })
 
--- frontend/src/views/TrainList.vue
+++ frontend/src/views/TrainList.vue
@@ -1,20 +1,40 @@
 <script setup lang="ts">
-import { inject } from "vue"
+import { inject, watch, ref, onUnmounted } from "vue"
 import { RouterLink } from "vue-router"
 
-const data = inject<any>("wsData")
+const data = inject<any>("mqttData")
+
+const trains = ref("--")
+const topic = "test/pico"
+
+watch(() => data?.value, (newData) => {
+  newData.subscribe(topic)
+  newData.on("message", function(topic, msg){
+    console.log(msg)
+    trains.value = msg.toString()
+  })
+  console.log(data)
+})
 
 console.log(data)
+
+onUnmounted(() => {
+  if(data?.value) {
+    data.value.unsubscribe(topic)
+    data.value.off("message")
+  }
+})
 
 </script>
 <template>
   <!-- <p>{{data}}</p> -->
   <h1>車両一覧</h1>
-  <ul v-if="data">
-    <li v-for="train in data.trains"> 
-      <RouterLink :to="`train-list/${train.id}`">
-        id: {{train.id}}, 現在位置：{{train.read_id}}
-      </RouterLink>
-    </li>
-  </ul>
+  <p>{{trains}}</p>
+  <!-- <ul v-if="data"> -->
+  <!--   <li v-for="train in data.trains">  -->
+  <!--     <RouterLink :to="`train-list/${train.id}`"> -->
+  <!--       id: {{train.id}}, 現在位置：{{train.read_id}} -->
+  <!--     </RouterLink> -->
+  <!--   </li> -->
+  <!-- </ul> -->
 </template>
