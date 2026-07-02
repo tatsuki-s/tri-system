cf1f8ca5973b2533e153a848f9f62a27468ab1f1
tatsuki-s <zekoushn@gmail.com>
2026-07-02 18:01:28 +0900

    style:

--- train/main.py
+++ train/main.py
@@ -1,7 +1,9 @@
 import machine
+import gc
 import network
 import uasyncio as asyncio
-from umqtt.simple import MQTTClient
+# from umqtt.simple import MQTTClient
+from mqtt_as import MQTT_base, config as mconf
 import config
 
 FORWARD_PIN = 6
@@ -20,13 +22,21 @@ VOLUME = [0, 1200, 2400, 5000, 6100, 8500, 11000, 13000, 15000, 17000, 19000, 60
 MOTER_STEP = [400, 300, 200, 100, 0, -100, -200, -300, -400, -500, -1500]
 WIFI_SSID = config.WIFI_SSID
 WIFI_PASS = config.WIFI_PASS
-MQTT_BROKER = config.MQTT_BROKER
-CLIENT_ID = 0
+# MQTT_BROKER = config.MQTT_BROKER
+CLIENT_ID = "0"
 TOPIC = b"trains" #暫定。今後"trains/CLIENT_ID"となる予定
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-client = MQTTClient(CLIENT_ID, MQTT_BROKER)
+# mconf["ssid"] = config.WIFI_SSID
+# mconf["wifi_pw"] = config.WIFI_PASS
+mconf["server"] = config.MQTT_BROKER
+mconf["client_id"] = CLIENT_ID
+mconf["keepalive"] = 60
+
+client = MQTT_base(mconf)
+client.DEBUG = True
+is_connected = False
 
 mqtt_data = {
     "id": CLIENT_ID,
@@ -42,7 +52,6 @@ async def drive(limit_duty):
     adc = machine.ADC(ADC_PIN)
     forward = machine.Pin(FORWARD_PIN,machine.Pin.IN,machine.Pin.PULL_UP)
     reverse = machine.Pin(REVERSE_PIN,machine.Pin.IN,machine.Pin.PULL_UP)
-    # reverse = False
     pwm = machine.PWM(machine.Pin(PWM_PIN))
     pwm.freq(2000)
     # Falseで正転，Trueで逆転
@@ -99,34 +108,43 @@ async def drive(limit_duty):
         pwm.duty_u16(duty)
         
         print(generate_step(mc_value), now_direc, duty, mc_value)
-        mqtt_data["speed"] = generate_step(mc_value)
+        mqtt_data["speed"] = duty
         mqtt_data["direction"] = now_direc 
-        mqtt_data["mc"] = mc_value
+        mqtt_data["mc"] = generate_step(mc_value)
         await asyncio.sleep(0.08)
 
 async def mqtt_task():
-    global mqtt_data
+    global mqtt_data, is_connected
     is_connected = False
+    # await client.connect()
 
     while True:
+        print(wlan.isconnected(), is_connected, gc.mem_free(), wlan.status())
         if not wlan.isconnected():
-            wlan.connect(WIFI_SSID, WIFI_PASS)
-            while not wlan.isconnected():
-                await asyncio.sleep(1)
             is_connected = False
+            wlan.connect(WIFI_SSID, WIFI_PASS)
+            for _ in range(5):
+                if wlan.isconnected():
+                    break
+                await asyncio.sleep(0.1)
+            if not wlan.isconnected():
+                await asyncio.sleep(1)
+                continue
+            # while not wlan.isconnected():
+                # await asyncio.sleep(5)
 
         #mqttの再接続
         if not is_connected:
             try:
-                client.connect()
+                await client.connect()
                 is_connected = True
             except Exception as e:
                 print("mqtt error", e)
-                await asyncio.sleep(1)
+                await asyncio.sleep(5)
                 continue
         try:
-                client.publish(TOPIC, str([mqtt_data]).encode()) #[]で囲っているのは暫定
-                await asyncio.sleep(1)
+            await client.publish(TOPIC, str([mqtt_data]).encode()) #[]で囲っているのは暫定
+            # await asyncio.sleep(5)
         except Exception as e:
             print("送信エラー", e)
             is_connected = False
