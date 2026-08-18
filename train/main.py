ccc22d7a72c7a5e63715db3fe43004bbe4067187
tatsuki-s <zekoushn@gmail.com>
2026-08-18 15:24:46 +0900

    feat:Wi-Fi接続が安定するよう修正

--- train/main.py
+++ train/main.py
@@ -31,8 +31,16 @@ READ_TOPIC = f"train/${CLIENT_ID}/limit"
 
 uart = machine.UART(0, baudrate=9600, tx=machine.Pin(0), rx=machine.Pin(1), timeout=10)
 
-# wlan = network.WLAN(network.STA_IF)
-# wlan.active(True)
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(config.WIFI_SSID, config.WIFI_PASS)
+    
+import time
+while not wlan.isconnected():
+    print("waiting for wifi...")
+    time.sleep(1)
+print("wifi connected:", wlan.ifconfig())
+
 mconf["ssid"] = config.WIFI_SSID
 mconf["wifi_pw"] = config.WIFI_PASS
 mconf["server"] = config.MQTT_BROKER
